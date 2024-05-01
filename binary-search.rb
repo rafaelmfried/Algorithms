29a4a9447df06955e9cd8f1a810c211d5bda0d17
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-05-01 12:28:11 -0300

    feat: create a binary search algorithm using ruby and poo

---
+++ binary-search.rb
@@ -0,0 +1,31 @@
+class BinarySearch
+    def initialize(array)
+        @array = array.sort
+    end
+
+    def search(target)
+        left = 0
+        right = @array.length - 1
+        while right >= left
+            middle = (right + left) / 2
+
+            if @array[middle] == target
+                puts "Target #{target} founded at index #{middle}"
+                return middle
+            elsif @array[middle] < target
+                left = middle + 1
+            else
+                right = middle - 1
+            end
+        end
+        puts "Target #{target} not founded at array"
+        return -1
+    end
+end
+
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+
+target = 5
+
+searcher = BinarySearch.new(array)
+searcher.search(target)
