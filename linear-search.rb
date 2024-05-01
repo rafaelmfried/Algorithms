1c303a06add3a2123ed20f0ed9d6d3280b2c34da
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-05-01 12:12:01 -0300

    feat: create a linear search class and a search method

---
+++ linear-search.rb
@@ -0,0 +1,32 @@
+# Linear Search Algorithm
+
+class LinearSearch
+    def initialize(array)
+        @array = array
+    end
+
+    def search(target)
+        found = false
+        index = nil
+
+        @array.each_with_index do |element, i|
+            if element == target
+                found = true
+                index = i
+                break
+            end
+        end
+
+        if found
+            puts "Target #{target} founded at index #{index}"
+        else
+            puts "Target #{target} not founded in array"
+        end
+    end
+end
+
+array = [10, 50, 70, 80, 30, 20, 90, 40]
+target = 30
+
+searcher = LinearSearch.new(array)
+searcher.search(target)
