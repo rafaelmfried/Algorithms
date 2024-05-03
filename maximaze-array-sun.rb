847e61e734d17e73cbe8753402cfad6fdb3ed484
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-05-02 22:16:03 -0300

    feat: maximize array after k negations, need to fix something

---
+++ maximaze-array-sun.rb
@@ -0,0 +1,27 @@
+# Maximaze Array Sum
+
+class MaximumSum
+    def self.maximum_sum(array, k)
+        array.sort!
+
+        index = 0
+
+        while k > 0 && index < array.length && array[index] > 0
+            array[index] = -array[index]
+            k -= 1
+            index += 1
+        end
+
+        if k % 2 != 0
+            minimum_value_index = array.index(array.min) 
+            array[minimum_value_index] = -array[minimum_value_index]
+        end
+
+        puts "The Maximum sum of the array #{array} After #{k} Negations is #{array.sum}"
+        return array.sum
+    end
+end
+
+array = [-2, 0, 5, -1, 2]
+k = 4
+MaximumSum.maximum_sum(array, k)
